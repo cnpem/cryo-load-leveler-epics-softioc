3ea3066cbcb07057632a2d94c500a61992fcea4a
Gustavo de Souza dos Reis <gustavo.reis@lnls.br>
2026-08-18 11:41:26 -0300

    Import dbs before IOC startup.

--- iocBoot/ioccryoLeveler/st.cmd
+++ iocBoot/ioccryoLeveler/st.cmd
@@ -2,11 +2,18 @@
 
 < envPaths
 
+epicsEnvSet("P", "SI-03SP:")
+epicsEnvSet("R", "RF-CryoMod-")
+epicsEnvSet("RF_R", "RF-SRFCav-")
+
 cd "${TOP}"
 
 ## Register all support components
-dbLoadDatabase "dbd/cryo-load-leveler.dbd"
-cryo_load_leveler_registerRecordDeviceDriver pdbbase
+dbLoadDatabase "dbd/cryoLeveler.dbd"
+cryoLeveler_registerRecordDeviceDriver pdbbase
+
+dbLoadRecords("$(TOP)/db/cll.db", "P=$(P), R=$(R), RF_R=$(RF_R), CRYO_MOD=1, SRF_CAV=A")
+dbLoadRecords("$(TOP)/db/cll.db", "P=$(P), R=$(R), RF_R=$(RF_R), CRYO_MOD=2, SRF_CAV=B")
 
 cd "${TOP}/iocBoot/${IOC}"
 iocInit
